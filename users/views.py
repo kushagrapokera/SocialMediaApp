11ef9ff40891cb855cf9c8e37ee82866b3a59193
kushagrapokera <kushagrapokera@gmail.com>
2025-01-20 11:10:45 +0530

    Added Login Functionality

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
+from  django.http import HttpResponse
 from django.shortcuts import render
+from  django.contrib.auth import authenticate,login
+from .forms import LoginForm
+
 
 # Create your views here.
+
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'],password=data['password'])
+            if user is not None:
+                login(request,user)
+                return HttpResponse("User Autrhnticated and Logged in")
+            else:
+                return HttpResponse('Invalid credentials')
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
